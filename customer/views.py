484cec957a89005d5a9481574718aef55e2e082b
ayushrko <33518505+ayushrko@users.noreply.github.com>
2020-03-27 13:19:08 +0530

    api call works

--- customer/views.py
+++ customer/views.py
@@ -1,5 +1,45 @@
 from django.shortcuts import render
+import requests
+from django.template.defaulttags import register
 
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
 
 def home(request):
+  if request.method == "POST":
+    search = request.POST.get('search')
+    print(search)
+    url = 'https://forkify-api.herokuapp.com/api/search?&q=%s' % search
+    print(url)
+    response = requests.get(url)
+    searchdata = response.json()
+    
+    recipes = searchdata['recipes']
+    posts = []
+    for k in recipes:
+        #print(k['title'])
+        posts.append(k['title']) 
+    
+    count = len(posts)
+    print(count)
+
+    for x in range(len(posts)): 
+        print (posts[x])   
+       
+    #resc = recipe[count-2]
+    #print(resc)
+    #res = resc['title']
+    #print(res)
+    #print(recipe[2]['title'])
+    
+
+    return render(request , 'customer\PrimeP.html' , { 'posts': posts , 'search':search } )
+  else:
     return render(request , 'customer\PrimeP.html')
+
+def home2(request):
+    response = requests.get('https://forkify-api.herokuapp.com/api/search?&q=pizza')
+    searchdata = response.json()
+    print(searchdata.recipes)
+    return render(request , 'customer\PrimeP.html' , {'count':searchdata['count'] , 'recipes':searchdata['title']})
